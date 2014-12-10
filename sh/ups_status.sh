72c283730aaae0e1701b2c23595c13b0338f03cd
allion <allion@users.noreply.github.com>
2014-12-10 10:04:11 +0300

    Fix SSL message "Init SSL without certificate database"

--- sh/ups_status.sh
+++ sh/ups_status.sh
@@ -6,7 +6,7 @@ if [ $ups = ups.discovery ]; then
 
     echo -e "{\n\t\"data\":["
     first=1
-    /bin/upsc -l | while read discovered ; do 
+    /bin/upsc -l 2>&1 | grep -v SSL | while read discovered ; do 
         if [ $first -eq 0 ]; then
             echo -e ","
         fi
@@ -19,7 +19,7 @@ else
 key=$2
 
 if [ $key = ups.status ]; then
-	state=`/bin/upsc $ups $key`
+	state=`/bin/upsc $ups $key 2>&1 | grep -v SSL`
 	case $state in
 		OL)		echo 1 ;; #'On line (mains is present)' ;;
 		OB)		echo 2 ;; #'On battery (mains is not present)' ;;
@@ -36,7 +36,7 @@ if [ $key = ups.status ]; then
 		* )		echo 0 ;; #'unknown state' ;;
 	esac
 else
-	/bin/upsc $ups $key
+	/bin/upsc $ups $key  2>&1 | grep -v SSL
 fi
 
 fi
